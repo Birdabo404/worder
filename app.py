a184fad9a80e29af2ab297dada7752918348aad3
Birdabo404 <Birdabo420@gmail.com>
2026-01-06 23:16:23 +0800

    i forgot how to read loop a file

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("test!")
+worder = None
+'''
+try: 
+    with open('word.txt') as file: 
+        print(file.read())
+except FileNotFoundError:
+    print("error: the file was not found. :<")
+'''
+
+for count in range():
+    try:
+        with open('words.txt') as file:
+            print(file.read())
+    except FileNotFoundError:
+        print("file not found! :<")
+
