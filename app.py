d859d892a05a4f9365a7fb39ad382dbeed4d8e59
Birdabo404 <Birdabo420@gmail.com>
2026-01-12 21:50:05 +0800

    fixed random chosen word to output.

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-
 import random 
 
 worder = None 
 
 #initialize word bank as empty.
-cleaned_lines = []
+word_Bank = []
 
 
 # load the file content into a list and loop through words.txt line by line.
 try: 
     with open('words.txt', 'r') as file:
         for line in file:
-            clean_line = line.rstrip()
+            clean_words = line.rstrip()
 
             #add clean lines to list.
-            cleaned_lines.append(clean_line)
+            word_Bank.append(clean_words)
     
 except FileNotFoundError:
         print("File was not found! :<")
 
 #should now print the words because i removed the readlines() function. 
-print(cleaned_lines)
+#print(word_Bank) :commented to remove printed word list. 
+
+random_Words = random.choice(word_Bank)
+print(f"The chosen word is: {random_Words}")
 
 
